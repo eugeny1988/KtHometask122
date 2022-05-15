1d64d60279b42e3297603126e15213c16aa599b0
Евгений <eugeny1988@gmail.com>
2022-05-15 13:39:02 +0300

    Modified conditions in if-else expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val likes = 1
-    val people = if ((likes === 1 || (likes - 1).mod(10) === 0) && likes != 11) "человеку"
+    val likes = 401
+    val people = if (((likes - 1).mod(10) === 0) && likes != 11) "человеку"
     else "людям"
     println("Понравилось $likes $people")
 }
